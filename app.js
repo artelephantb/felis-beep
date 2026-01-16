1f88441640de983e9f48437cf91667c173a9cf06
Artitapheiont <87944666+artelephantb@users.noreply.github.com>
2026-01-16 07:00:37 -0500

    Added switch button

--- app.js
+++ app.js
@@ -2,22 +2,58 @@ const codeTabReference = document.getElementById('tab:r0:0');
 const costumesTabReference = document.getElementById('tab:r0:1');
 const soundsTabReference = document.getElementById('tab:r0:2');
 
-var currentTab = 0;
+const soundsPanelReference = document.getElementById('panel:r0:2');
 
+var currentTab = 0;
+var isBeepBoxOpened = false;
+
+
+function createBeepBoxUI() {
+	const soundsEditorReference = document.getElementsByClassName('sound-editor_editor-container_bd-4K')[0];
+	if (soundsEditorReference == undefined) {
+		setTimeout(createBeepBoxUI, 5);
+		return;
+	}
+
+	const switchButton = document.createElement('button');
+	switchButton.className = 'switch-button';
+	switchButton.innerText = 'Switch Mode';
+
+	switchButton.addEventListener('click', onSwitchButtonClicked);
+
+
+	soundsEditorReference.appendChild(switchButton);
+}
+
+
+function onSwitchButtonClicked() {
+	const soundsEditorReference = document.getElementsByClassName('sound-editor_editor-container_bd-4K')[0];
+
+	const soundTitleReference = soundsEditorReference.childNodes[0];
+	const soundWaveReference = soundsEditorReference.childNodes[1];
+	const soundControlsReference = soundsEditorReference.childNodes[2];
+
+
+	if (isBeepBoxOpened) {
+		soundTitleReference.style.display = '';
+		soundWaveReference.style.display = '';
+		soundControlsReference.style.display = '';
+	} else {
+		soundTitleReference.style.display = 'none';
+		soundWaveReference.style.display = 'none';
+		soundControlsReference.style.display = 'none';
+	}
+
+	isBeepBoxOpened = !isBeepBoxOpened;
+}
 
 function onTabChanged(newTab) {
 	if (newTab == currentTab) return;
 
 	currentTab = newTab;
-	if (newTab == 0) {
-		alert('Code tab pressed!');
-	} else if (newTab == 1) {
-		alert('Costumes tab pressed!');
-	} else if (newTab == 2) {
-		alert('Audio tab pressed!');
-	} else {
-		alert('Unknown tab');
-	}
+	if (newTab != 2) return;
+
+	createBeepBoxUI();
 }
 
 
